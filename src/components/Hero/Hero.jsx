52bd68cf36c216a3d2d5ba98f00be8d946ae47bf
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-10 19:19:44 +0100

    💄 F03: add icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import OrangeImg from "../../assets/orangebg.png"
 import NavBar from "../NavBar/NavBar.jsx";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
   const [sideBar, setSideBar] = useState(false)
@@ -51,7 +52,18 @@ const Hero = () => {
               <div className="w-full h-full flex justify-center items-center">
                 <div className="text-white flex flex-col justify-center items-center gap-6">
                   {/* line */}
-                  
+                  <div className="w-[1px] h-[70px] bg-white"></div>
+                  <div className="inline-block p-2 cursor-pointer">
+                    <FaFacebook className="text-2xl hover:text-green-500 "/>
+                  </div>
+                  <div className="inline-block p-2 cursor-pointer">
+                    <FaInstagram className="text-2xl hover:text-green-500 "/>
+                  </div>
+                  <div className="inline-block p-2 cursor-pointer">
+                    <FaLinkedin className="text-2xl hover:text-green-500"/>
+                  </div>
+                  {/* line */}
+                  <div className="w-[1px] h-[70px] bg-white"></div>
                 </div>
               </div>
             </div>
